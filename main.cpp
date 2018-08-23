1de82bc8b3cc331217f5be8efd08de4f48bf6b11
vinti9 <eoliveiral@hotmail.com>
2018-08-23 01:46:59 -0300

    Terceiro commit

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     cout << "Mudou commit" << endl;
-    cout << "Segundo commit" << endl;
+    cout << "Segundo" << endl;
+    cout << "Terceiro commit" << endl;
     return 0;
 }
