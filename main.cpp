9bbe6214da137fc63a0642914bbe922da76443a6
vinti9 <eoliveiral@hotmail.com>
2018-08-23 02:15:47 -0300

    Salvar

--- main.cpp
+++ main.cpp
@@ -2,14 +2,11 @@
 using namespace std;
 
 int main(int argc, char const *argv[])
-{
-    cout << "Mudou commit" << endl;
-    cout << "Segundo" << endl;
-    
+{   
     cout << "Inteiros: " << endl;
     for(size_t i = 0; i < 10; i++)
     {
-        cout << i << endl;
+        cout << i;
     }
     
     return 0;
