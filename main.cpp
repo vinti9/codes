332e29fe9e0450101f0a972a914b8ae75c017f63
vinti9 <eoliveiral@hotmail.com>
2018-08-23 02:11:59 -0300

    Removido

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@ int main(int argc, char const *argv[])
 {
     cout << "Mudou commit" << endl;
     cout << "Segundo" << endl;
-
     
     cout << "Inteiros: " << endl;
     for(size_t i = 0; i < 10; i++)
