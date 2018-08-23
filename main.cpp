918ad6fd8fba02cd909d138f0a3640d4789201e6
vinti9 <eoliveiral@hotmail.com>
2018-08-23 01:57:14 -0300

    Vamos lá

--- main.cpp
+++ main.cpp
@@ -6,5 +6,6 @@ int main(int argc, char const *argv[])
     cout << "Mudou commit" << endl;
     cout << "Segundo" << endl;
     cout << "Terceiro commit" << endl;
+    cout << "Vamos lá" << endl;
     return 0;
 }
